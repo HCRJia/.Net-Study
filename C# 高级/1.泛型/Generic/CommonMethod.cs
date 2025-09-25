919ed6c5cdb41f7b4df878dad35692cc6218ba83
HCRJia <2441919651@qq.com>
2025-09-25 21:57:06 +0800

    更新泛型（部分）

---
+++ C# 高级/1.泛型/Generic/CommonMethod.cs
@@ -0,0 +1,61 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Generic
+{
+    public class CommonMethod
+    {
+
+        /// <summary>
+        /// 打印个int值
+        /// </summary>
+        /// <param name="iParameter"></param>
+        public static void ShowInt(int iParameter)
+        {
+            Console.WriteLine("This is {0},parameter={1},type={2}",
+                typeof(CommonMethod).Name, iParameter.GetType().Name, iParameter);
+        }
+        /// <summary>
+        /// 打印个string值
+        /// </summary>
+        /// <param name="sParameter"></param>
+        public static void ShowString(string sParameter)
+        {
+            Console.WriteLine("This is {0},parameter={1},type={2}",
+                typeof(CommonMethod).Name, sParameter.GetType().Name, sParameter);
+        }
+        /// <summary>
+        /// 打印个DateTime值
+        /// </summary>
+        /// <param name="oParameter"></param>
+        public static void ShowDateTime(DateTime dtParameter)
+        {
+            Console.WriteLine("This is {0},parameter={1},type={2}",
+                typeof(CommonMethod).Name, dtParameter.GetType().Name, dtParameter);
+        }
+
+        /// <summary>
+        /// 打印个object值
+        /// 1 任何父类出现的地方，都可以用子类来代替
+        /// 2 object是一切类型的父类
+        /// 
+        /// 2个问题：
+        /// 1 装箱拆箱
+        /// </summary>
+        /// <param name="oParameter"></param>
+        public static void ShowObject(object oParameter)
+        {
+            Console.WriteLine("This is {0},parameter={1},type={2}",
+                typeof(CommonMethod), oParameter.GetType().Name, oParameter);
+        }
+
+        public static void Show<T>(T tParameter)//, T t = default(T
+        {
+            Console.WriteLine("This is {0},parameter={1},type={2}",
+               typeof(CommonMethod), tParameter.GetType().Name, tParameter);
+        }
+    }
+}
---
+++ C# 高级/1.泛型/Generic/Program.cs
@@ -0,0 +1,24 @@
+﻿using Generic;
+
+int iValue = 123;
+string sValue = "456";
+DateTime dtValue = DateTime.Now;
+object oValue = "678";
+
+Console.WriteLine("***********************Common***********************");
+CommonMethod.ShowInt(iValue);
+//CommonMethod.ShowInt(sValue);
+CommonMethod.ShowString(sValue);
+CommonMethod.ShowDateTime(dtValue);
+
+Console.WriteLine("***********************Object***********************");
+CommonMethod.ShowObject(iValue);
+CommonMethod.ShowObject(sValue);
+CommonMethod.ShowObject(dtValue);
+
+Console.WriteLine("***********************Generic***********************");
+CommonMethod.Show<int>(iValue);//调用泛型，需要指定类型参数
+//CommonMethod.Show(iValue);//如果可以从参数类型推断，可以省略类型参数---语法糖(编译器提供的功能)
+CommonMethod.Show<string>(sValue);
+//CommonMethod.Show<int>(sValue);//报错，因为类型错了
+CommonMethod.Show<DateTime>(dtValue);
