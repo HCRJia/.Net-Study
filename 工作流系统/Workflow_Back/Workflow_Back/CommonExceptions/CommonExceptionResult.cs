ee35b126ef2da8293e4da6f55198c7dbba4e7157
HCRJia <2441919651@qq.com>
2026-02-07 21:02:12 +0800

    工作流项目更新

--- 工作流系统/Workflow_Back/Workflow_Back/CommonExceptions/CommonExceptionResult.cs
+++
@@ -1,9 +0,0 @@
-﻿namespace Workflow_Back.CommonExceptions
-{
-    public class CommonExceptionResult
-    {
-        public string ErrorNo { set; get; } // 结果状态编号
-        public string ErrorInfo { set; get; } //  结果状态信息
-        public dynamic ErrorReason { set; get; } // 代表所有的Action结果
-    }
-}
--- 工作流系统/Workflow_Back/Workflow_Back/CommonResults/CommonResult.cs
+++
@@ -1,8 +0,0 @@
-﻿namespace Workflow_Back.CommonResults
-{
-    public class CommonResult
-    {
-        public string ErrorNo { set; get; } // 结果状态编号
-        public string ErrorInfo { set; get; } //  结果状态信息
-    }
-}
